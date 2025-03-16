449f2d587113fe75120b7d6fa9ff25f316767c1b
Madhu <enometh@net.meer>
2025-03-16 08:18:13 +0530

    plugins/remote-play.lisp: fix call to uiop:run-program

--- plugins/remote-play.lisp
+++ plugins/remote-play.lisp
@@ -6,6 +6,6 @@
 
 
 (defun remote-play (host play-program sound-name)
-  (run-in-shell "rsh" (format nil "~A ~A ~A" host play-program sound-name)))
+  (uiop:run-program `("rsh"  ,host ,play-program ,sound-name)))
 
 ;;; (remote-play "cmi1" "sfplay" (with-sound (:play nil) (fm-violin 0 1 440 .1)))
